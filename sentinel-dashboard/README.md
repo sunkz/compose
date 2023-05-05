d413e74867124bbda802bc9440fa8caac5ae576e
sunkz <sunkezheng@outlook.com>
2023-05-05 15:50:24 +0800

    2023-05-05 15:50:24

---
+++ sentinel-dashboard/README.md
@@ -0,0 +1,5 @@
+### [Sentinel](https://sentinelguard.io)  build in docker
+```text
+sentinel-dashboard : http://localhost:8081
+用户名/密码 : sentinel
+```
---
+++ sentinel-dashboard/build.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+docker build -t sunkezheng/sentinel-dashboard .
+docker push sunkezheng/sentinel-dashboard:latest
+docker rmi $(docker images -f "dangling=true" -q)
